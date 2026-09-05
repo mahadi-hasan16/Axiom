3f82d9a4def70df138efd98d1f3ad9103bc91b49
mahadi-hasan16 <mahadi.hasan.khan16@gmail.com>
2026-09-06 01:01:37 +0600

    configured database connection

--- api/src/main/java/com/axiom/api/ApiApplication.java
+++ api/src/main/java/com/axiom/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.axiom.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.load();
+
+		dotenv.entries().forEach(e ->
+				System.setProperty(e.getKey(), e.getValue())
+		);
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
